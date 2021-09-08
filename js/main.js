04b0f4eba2cbb5152bf45742d9200f95d2f26bb4
Rafael C. Santos <fael3000net.rcs@gmail.com>
2021-09-08 14:06:23 -0300

    subindo arquivos

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Rafael Santos ";
+var idade = 29;//*mutiplicar +soma -subritrair
+var idade2 = 10;
+var frase = "japão é o melhor time do mundo";
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2)
+console.log(frase.replace("japão", "brasil"));
+//alert(frase.replace("japão", "brasil"));
+//console.log(frase.toUpperCase);
+//console.log(frase.tolowercase);
