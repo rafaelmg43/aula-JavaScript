01def965c8af16d481f87fc835387920ba90397d
Rafael C. Santos <fael3000net.rcs@gmail.com>
2021-09-13 12:18:40 -0300

    array e dicionario

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
-var nome = "Rafael Santos ";
-var idade = 29;//*mutiplicar +soma -subritrair
-var idade2 = 10;
-var frase = "japão é o melhor time do mundo";
+var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}];//lista de DICIONARIO
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"};// DICIONARIO
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+
+//var lista = ["maça", "pêra", "laranja"];
+
+//lista.push("uva");//push coloca 
+//lista.pop();//retira
+//console.log(lista[0]);// array 
+//console.log(lista.toString()[0]);//imprime uma letra da string
+//console.log(lista.join(" * "));//muda os espacamentos e caracteres da string
+//console.log(lista.reverse());//inverte os itens da listados
+//console.log(lista.length);//tamanho da lista
+//console.log(lista);//lista os produtos
+//console.log(lista[1]);//mostra apenas o item pedido
+//alert(lista[1]);
+
+//var nome = "Rafael Santos ";
+//var idade = 29;//*mutiplicar +soma -subritrair
+//var idade2 = 10;
+//var frase = "japão é o melhor time do mundo";
 //alert(nome + "tem" + idade + "anos");
 //alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2)
-console.log(frase.replace("japão", "brasil"));
+//console.log(nome);
+//console.log(idade + idade2)
+//console.log(frase.replace("japão", "brasil"));
 //alert(frase.replace("japão", "brasil"));
 //console.log(frase.toUpperCase);
 //console.log(frase.tolowercase);
